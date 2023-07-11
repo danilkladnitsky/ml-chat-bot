48d7fdd9b1bae56364b6f6d3bfe2de64e60a377f
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-07-12 00:17:03 +0300

    ml-service: add websocket

--- ml-service/src/modules/ml/ml.controller.ts
+++ ml-service/src/modules/ml/ml.controller.ts
@@ -1,8 +1,12 @@
+import { Controller, Inject } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { MlService } from './ml.service';
 
+@Controller()
 export class MlController {
+  constructor(@Inject(MlService) private readonly mlService: MlService) {}
   @MessagePattern({ cmd: 'ping' })
   async ping() {
-    return 'pong';
+    return this.mlService.pingClients();
   }
 }
--- ml-service/src/modules/ml/ml.service.ts
+++ ml-service/src/modules/ml/ml.service.ts
@@ -1,1 +1,33 @@
-export class MlService {}
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway(+process.env.ML_PORT)
+export class MlService
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  io: Server;
+
+  async pingClients() {
+    this.io.emit('ping', 'pong');
+    return 'pong';
+  }
+
+  afterInit(server: any) {
+    console.log('websocket is ready');
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('connection');
+  }
+
+  handleDisconnect(client: any) {
+    console.log('disconnect');
+  }
+}
