475f046e0918dbbcd1bf38d69217e423eec8b8f9
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-07-12 02:58:43 +0300

    frontend: fix typo

--- frontend/src/socket/index.ts
+++ frontend/src/socket/index.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 const URL = ENV_VARS.SOCKET_HOST;
-const CURRENT_URL = window.location.href.split('https//')[1];
+const CURRENT_URL = window.location.href.split('https://')[1];
 
 export const socket = io(URL || `wss://${CURRENT_URL}`);
